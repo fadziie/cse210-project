9f8658f4a785092ee73cb850b92245796e289f8a
fadziie <knxtfdzw@gmail.com>
2023-02-25 23:51:18 +0200

    completed prep number 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guessedNumber = 23;
+
+        while (guessedNumber != number)
+        {
+            Console.Write("What is your guess? ");
+            guessedNumber = int.Parse(Console.ReadLine());
+
+            if (number > guessedNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guessedNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            
+        }
+    }   
 }
