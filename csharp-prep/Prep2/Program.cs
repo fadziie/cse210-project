955e9348f8ddf0e9f364b7cfe9823ff793a600b1
fadziie <knxtfdzw@gmail.com>
2023-02-25 23:14:31 +0200

    completed prep number 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your mark");
+        string valueFromUser = Console.ReadLine();
+        int mark = int.Parse(valueFromUser);
+
+        string grade = "";
+
+        if (mark <= 90)
+        {
+          grade = "A" ; 
+        }
+        else if (mark >= 80)
+        {
+            grade = "B";
+        }
+        else if (mark >= 70)
+        {
+            grade = "C";
+        }
+        else if (mark >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is: {grade}");
+        
+        if (mark >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
