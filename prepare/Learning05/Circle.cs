c9e7d28b95e3762805d8f815492a7b08caf763f5
fadziie <knxtfdzw@gmail.com>
2023-03-21 21:47:16 +0200

    Learning ACtivity Complete

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Circle : Shape
+{
+    public double _radius;
+    public Circle (string color,double radius) : base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return (3.14) * Math.Pow(_radius, 2);
+    }
+}
